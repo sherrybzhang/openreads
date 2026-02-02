167615e674d1e511049a41779899a9c9affac91c
Sherry Zhang <zsherry168@gmail.com>
2026-02-01 19:15:07 -0500

    refactor(scripts): update import path

--- scripts/import_books.py
+++ scripts/import_books.py
@@ -1,4 +1,5 @@
 import csv
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.sql import text
@@ -9,7 +10,8 @@ engine = create_engine('postgresql://localhost/sherryzhang')
 db = scoped_session(sessionmaker(bind=engine))
 
 def main():
-    f = open("books.csv")
+    data_path = os.path.join(os.path.dirname(__file__), "..", "data", "books.csv")
+    f = open(data_path)
     reader = csv.reader(f)
     for isbn, title, author, year in reader:
         db.execute(text("INSERT INTO books (isbn, title, author, year) VALUES (:isbn, :title, :author, :year)"),
